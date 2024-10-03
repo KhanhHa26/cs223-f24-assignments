4c0921783d8860be04a8dc2dca42671038d7962b
Khanh Ha Nguyen <knguyen1@goldengate.cs.brynmawr.edu>
2024-10-03 19:49:46 -0400

    assignment complete

--- A05/crossword.c
+++ A05/crossword.c
@@ -1,10 +1,67 @@
 /*----------------------------------------------
- * Author: 
- * Date: 
- * Description
+ * Author: Khanh Ha Nguyen
+ * Date: 10/02/2024
+ * Description: This program generates a crossword using two words from a user.
  ---------------------------------------------*/
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main() {
+int main(int argc, char* argv[]) {
+  if (argc < 3) {
+    printf("Usage: %s <word1> <word2>\n", argv[0]);
+    return 1;
+  } 
+
+  char* word1 = argv[1];  // First input word
+  char* word2 = argv[2];  // Second input word
+  int row = -1;
+  int col = -1;
+  char *arr = (char *)malloc(sizeof(char) * strlen(word1) * strlen(word2));
+
+  //Find the common char
+  for (int i = 0; i < strlen(word1); i++) {
+    for (int j = 0; j < strlen(word2); j++) {
+      if (i != j && word1[i] == word2[j]) { 
+        col = i;
+        row = j;
+      }
+
+      //if found the common character
+      if (col != -1 && row != -1) {
+        break;
+      }
+    }
+  }
+
+  //If there is no matching char
+  if (col == -1 || row == -1) {
+    printf("No common letter!\n");
+    return 1;
+  }
+
+  //Create the output 
+  for (int i = 0; i < strlen(word1); i++) {
+    for (int j = 0; j < strlen(word2); j++) {
+      if (j == row) {
+        arr[i*strlen(word2)+j] = word1[i];
+      } else if (i == col) {
+        arr[i*strlen(word2)+j] = word2[j];
+      } else {
+        arr[i*strlen(word2)+j] = '.';
+      }
+    }
+  }
+
+  // Print the array
+  for (int i = 0; i < strlen(word1); i++) {
+    for (int j = 0; j < strlen(word2); j++) {
+      printf("%c ", arr[i * strlen(word2) + j]);
+    }
+    printf("\n");
+  }
+
+  free(arr);
   return 0;
 }
+
--- A05/write_ppm.c
+++ A05/write_ppm.c
@@ -1,7 +1,7 @@
 /*----------------------------------------------
- * Author: 
- * Date: 
- * Description
+ * Author: Khanh Ha Nguyen
+ * Date: 10/3/2024
+ * Description: This program writes PPM in binary into the file 
  ---------------------------------------------*/
 #include "write_ppm.h"
 #include <stdio.h>
@@ -10,9 +10,18 @@
 // implement *one* (but do not remove the other one)!
 
 void write_ppm(const char* filename, struct ppm_pixel* pixels, int w, int h) {
-
+    FILE *output = fopen(filename, "w");
+    if (!output) {
+      printf("Error opening file");
+    }
+    fprintf(output, "P6\n");
+    fprintf(output, "%d %d\n", w, h);
+    fprintf(output, "255\n"); 
+    fwrite(pixels, sizeof(struct ppm_pixel), (w * h), output);
+    fclose(output);
 }
 
 void write_ppm_2d(const char* filename, struct ppm_pixel** pixels, int w, int h) {
 
 }
+
