745d0ec5f48ac55734e1a0c939e02a39ab712a1c
Khanh Ha Nguyen <knguyen1@goldengate.cs.brynmawr.edu>
2024-10-14 17:29:18 -0400

    assignment complete

--- A06/encode.c
+++ A06/encode.c
@@ -1,7 +1,7 @@
 /*----------------------------------------------
- * Author: 
- * Date: 
- * Description
+ * Author: Khanh Ha Nguyen
+ * Date: 10/13/2024
+ * Description: asks the user for a message to embed within it.
  ---------------------------------------------*/
 #include <stdio.h>
 #include <string.h>
@@ -11,10 +11,62 @@
 
 int main(int argc, char** argv) {
   if (argc != 2) {
-    printf("usage: encode <file.ppm>\n");
+    printf("usage: decode <file.ppm>\n");
     return 0;
   }
 
+  int width, height;
+  struct ppm_pixel *pixels = read_ppm(argv[1], &width, &height);
+
+  if (!pixels) {
+    printf("Error: Could not read PPM file.\n");
+    return 1;
+  }
+
+  printf("Reading %s with width %d and height %d\n", argv[1], width, height);
+  int max_chars = (width * height * 3) / 8; 
+  printf("Max number of characters in the image: %d\n", max_chars);
+  char* user_input = malloc(sizeof(char) * max_chars);
+  printf("Enter a phrase: ");
+  scanf("%s", user_input);
+
+  int message_length = strlen(user_input);
+  if (message_length > max_chars) {
+    printf("Error: Message too long to fit in the image.\n");
+    free(pixels);
+    return 1;
+  }
+
+  unsigned char mask = 0b1; 
+  unsigned char bit;
+
+  for (int i = 0; i < max_chars * 8; i++) {
+    //Extract current bit from phrase to encode it
+    if (i >= strlen(user_input) * 8){
+      bit = 0;
+    } else {
+      bit = (user_input[i / 8] >> (7 - i % 8)) & 0b1;
+    }
+    //Set new color value by updating the LSB
+    ((unsigned char*)pixels)[i] = (((unsigned char*)pixels)[i] & ~mask) | bit;
+  }
+
+  char output_filename[256];
+  strcpy(output_filename, argv[1]);
+
+  if (output_filename == NULL) {
+    printf("No file output");
+    return 1;
+  }
+
+  char *dot = strrchr(output_filename, '.');
+  strcpy(dot, "-encoded.ppm");
+  write_ppm(output_filename, pixels, width, height);
+
+  printf("Writing file %s\n", output_filename);
+  free(pixels);
+  free(user_input);
   return 0;
 }
+
 
--- A06/read_ppm.c
+++ A06/read_ppm.c
@@ -1,8 +1,9 @@
 /*----------------------------------------------
- * Author: 
- * Date: 
- * Description
+ * Author: Khanh Ha Nguyen
+ * Date: 10/3/2024
+ * Description: This program read PPM files stored in binary format
  ---------------------------------------------*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -11,9 +12,44 @@
 // Choose *one* to implement (do not remove the other one!)
 
 struct ppm_pixel* read_ppm(const char* filename, int* w, int* h) {
-  return NULL;
+  FILE *input = fopen(filename, "r");
+  int maxVal;
+
+  if (!input) {
+    printf("Error opening file");
+     return NULL;
+  }
+
+  char header[4];
+  fgets(header, sizeof(header), input); // read the magic number 
+    
+  //skip everything from # to \n
+  char c;
+  c = fgetc(input);
+  if(c=='#'){
+    do{
+      c = fgetc(input);
+    }while(c!='\n');
+  }
+  ungetc(c,input);
+
+  fscanf(input, "%d %d\n%d", w, h, &maxVal); //read width and height and max val
+  struct ppm_pixel *pixels = malloc((*w * *h) * sizeof(struct ppm_pixel));
+
+  //if memory allocation fails
+  if (!pixels) {
+    printf("Error: Memory allocation failed!\n");
+    fclose(input);
+    return NULL;
+  }
+
+  //read into the array
+  fread(pixels, sizeof(struct ppm_pixel),(*w * *h), input);
+  fclose(input);
+  return pixels;
 }
 
 struct ppm_pixel** read_ppm_2d(const char* filename, int* w, int* h) {
   return NULL;
 }
+
